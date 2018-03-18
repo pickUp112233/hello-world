3eff6a279f43468e155bdb99a7dca3719da50b56
wangzhihui <1301900713@qq.com>
2018-03-18 20:07:10 +0800

    ll

---
+++ 123/src/com/wzh/entity/Person.java
@@ -0,0 +1,46 @@
+package com.wzh.entity;
+
+public class Person {
+
+	private Integer id;
+	private String name;
+	private String gender;
+	private String address;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("start");
+
+	}
+}
